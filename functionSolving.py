10b3adb0cc85a0637cad3f8f1567e1ab12a67106
wfrasunku <afrasun@wp.pl>
2024-05-26 16:24:46 +0200

    Commit #3
    Smth is, however doesnt work as intended

--- functionSolving.py
+++ functionSolving.py
@@ -2,24 +2,59 @@ import mathLib
 import math
 
 
-def gauss_chebyshev(functions: list, nodes_amount: int, data: list) -> float:
+def gauss_chebyshev(functions: list, nodes_amount: int, data: list, n: int) -> float:
     result = 0
     for i in range(nodes_amount):
         w = data[i][0]
         x = data[i][1]
-        result += w * mathLib.evaluate_composite(x, functions)
+        result += w * mathLib.evaluate_composite(x, functions) * approx_polynomial(n, x)
     return result
 
 
-def approx_polynomial(n: int, x: float) -> float:
+def approx_polynomial(n: int, x: float):
     """
     :param n: stopień wielomianu (int)
     :param x: argument funkcji (float)
     :return: Wielomian Czebyszewa dla x stopnia n
     """
     result = [1, x]
-    for n in range(2, n):
+    for n in range(2, n + 1):
         result.append(2 * x * result[n - 1] - result[n - 2])
     return result[n]
 
 
+def wsp_apro(functions: list, nodes_amount: int, data: list, n: int):
+    """
+    :param wybor_funkcji: -//-
+    :param liczba_wezlow: -//-
+    :param n: -//-
+    :return: wspołczynnik wielomianu aproksymujacego
+    """
+    wsp = (2 * n) / 2 * gauss_chebyshev(functions, nodes_amount, data, n)
+    return wsp
+
+
+def wsp_wielomian(functions: list, nodes_amount: int, data: list, n: int):
+    """
+    :param wybor_funkcji: -//-
+    :param liczba_wezlow: -//-
+    :param n: -//-
+    :return: lista wspołczynników wielomianu aproksymującego
+    """
+    wielomian = []
+    for i in range(n):
+        wielomian.append(wsp_apro(functions, nodes_amount, data, i))
+    return wielomian
+
+
+def wart_wielomian(n, x, tab_wsp):
+    """
+    :param n: -//-
+    :param x: -//-
+    :param tab_wsp: -//-
+    :return: wartość wielomianu aproksymującego dla argumentu x
+    """
+    poly = 0
+    for i in range(n):
+        poly += tab_wsp[i] * approx_polynomial(i, x)
+    return poly
--- main.py
+++ main.py
@@ -10,27 +10,27 @@ import matplotlib.image as mpimg
 
 
 def main():
-    table_data = []
     file_path = 'chebyshev.txt'
     functions_list = menu.functions_menu()
     right_boundary, left_boundary, nodes_value = menu.interval_menu()
-    approx = menu.approximation_menu()
+    data = menu.read_data(file_path, nodes_value)
+    n = menu.approximation_menu()
+    tab_wsp = functionSolving.wsp_wielomian(functions_list, nodes_value, data, n)
 
     plt.rcParams['figure.figsize'] = config.PLOT_FIGURE_SIZE
     plt.rcParams['figure.autolayout'] = True
     plt.rcParams['figure.dpi'] = config.PLOT_FIGURE_DPI
 
     x_plot = numpy.linspace(left_boundary, right_boundary, 1000)
-    # y_lagrange = lagrange_polynomial(x_plot)
-    # y_lagrange_nodes = [lagrange_polynomial(n) for n in nodes]
+    y_gauss = functionSolving.wart_wielomian(n, x_plot, tab_wsp)
 
-    # plt.plot(x_plot, y_lagrange, color=config.PLOT_LAGRANGE_COLOR, label='Interpolacja Lagrange\'a', linewidth=2)
+    plt.plot(x_plot, y_gauss, color=config.PLOT_LAGRANGE_COLOR, label='Interpolacja Lagrange\'a', linewidth=2)
     y_poly: list[float] = [mathLib.evaluate_composite(e, functions_list) for e in x_plot]
     plt.plot(x_plot, y_poly, color=config.PLOT_POLY_COLOR, label="Oryginalna funkcja", linestyle='dashed')
 
     plt.xlabel('x', fontsize=config.PLOT_LABEL_FONT_SIZE)
     plt.ylabel('f(x)', fontsize=config.PLOT_LABEL_FONT_SIZE)
-    plt.title('Interpolacja Lagrange\'a', fontsize=config.PLOT_TITLE_FONT_SIZE)
+    plt.title('Ekstrapolacja Czybyszew\'a', fontsize=config.PLOT_TITLE_FONT_SIZE)
     plt.legend()
 
     plt.grid(True)
